de27cac498f3f921457f25e2f497948252f25f36
biswajit-git <biswajitm.17s3@gmail.com>
2019-10-20 15:15:53 +0530

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,16 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
+    debt=initialLevelOfDebt
+    rp=repaymentPercentage
+    ip=interestPercentage
+    repay=(debt*rp)/100
+    initdebt=debt
+    count=0
+    while debt>50:
+        debt=debt*(1+ip/100)-repay
+        count+=1
+
+    final=count*repay+0.1*initdebt+debt
+    answer =int(final)
     return answer
